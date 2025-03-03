54473350041d183c9dbe98118ba59a96307cccfa
RadyaI <radyaiftikhar@gmail.com>
2025-03-03 20:55:51 +0700

    Update content style

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
       <div className="w-9/12 p-5 mx-auto mt-8">
         <p className="text-center text-5xl font-bold tracking-wider font-[chewy]">Rate anything you want</p>
       </div>
-      <Suspense fallback={<div className="text-center">Loading...</div>}>
+      <Suspense fallback={<div className="text-center">Sabar yaa...</div>}>
         <Content />
       </Suspense>
     </>
--- components/root/content.tsx
+++ components/root/content.tsx
@@ -3,7 +3,7 @@
 export default function Content() {
     return (
         <>
-            <div className="w-[80%] mx-auto mt-4 p-10 border border-black"></div>
+            <div className="w-[90%] mx-auto mt-4 p-10 shadow"></div>
         </>
     )
 }
