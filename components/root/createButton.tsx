afa85291b0ec7c815e2ee39debf7a1e8287c7e4e
RadyaI <radyaiftikhar@gmail.com>
2025-03-04 18:03:00 +0700

    Menu update

--- components/root/createButton.tsx
+++ components/root/createButton.tsx
@@ -1,5 +1,13 @@
 export default function CreateButton() {
-    return(
-        <button className="bg-black transition-all text-white fixed top-2 right-2 cursor-pointer hover:bg-[#F1F0E9] border-2 hover:text-black hover:border-black px-5 py-2 rounded-lg">Create</button>
+
+    
+
+    return (
+        <>
+            <div className="fixed p-4 top-2 right-2 flex flex-col items-center">
+                <div className="w-12 h-12 rounded-full cursor-pointer border border-black"></div>
+                <button className="bg-black transition-all mt-4 text-white cursor-pointer hover:bg-[#F1F0E9] border-2 hover:text-black hover:border-black px-5 py-2 rounded-lg">Create</button>
+            </div>
+        </>
     )
 }
