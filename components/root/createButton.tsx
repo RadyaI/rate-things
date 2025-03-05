95c7d853c5cfa2adba19b7a9d01fee8b667211e7
RadyaI <radyaiftikhar@gmail.com>
2025-03-05 21:29:23 +0700

    Feat: google login

--- components/root/createButton.tsx
+++ components/root/createButton.tsx
@@ -1,19 +1,69 @@
 "use client"
 
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { UserOutlined } from "@ant-design/icons"
+import { FcGoogle } from "react-icons/fc";
+import { GoogleAuthProvider, onAuthStateChanged, signInWithPopup, signOut } from "firebase/auth";
+import { auth } from "@/config/firebase";
+import { toast, ToastContainer } from "react-toastify";
+import swal from "sweetalert";
 
 export default function CreateButton() {
     const [toggle, setToggle] = useState(false);
+    const [isLoggedIn, setIsLoggedIn] = useState(true);
+
+    useEffect(() => {
+        const subs = onAuthStateChanged(auth, (user) => {
+            if (user) {
+                setIsLoggedIn(true)
+            } else {
+                setIsLoggedIn(false)
+            }
+        })
+
+        return () => subs()
+    }, [])
+
+    async function loginGoogle() {
+        try {
+            const provider = new GoogleAuthProvider()
+            await signInWithPopup(auth, provider)
+
+            toast.success("Login successfully.")
+        } catch (error: any) {
+            console.log(error.message)
+            toast.error("Something went wrong.")
+        }
+    }
+
+    async function logOut() {
+        try {
+            await signOut(auth)
+            toast.success("Logout successfully")
+        } catch (error: any) {
+            console.log(error.message)
+            toast.error("Something went wrong.")
+        }
+    }
 
     return (
-        <div className="fixed w-34 p-4 top-2 right-2 flex flex-col items-center">
-            <div onClick={() => setToggle(!toggle)} className="w-12 h-12 rounded-full cursor-pointer bg-black text-white flex justify-center items-center"><UserOutlined className="text-2xl" /></div>
+        <>
+            <ToastContainer
+                position="bottom-right"
+                theme="dark"
+            />
+            <div className="fixed w-34 p-4 top-2 right-2 flex flex-col items-center">
+                {!isLoggedIn && (<div onClick={() => loginGoogle()} className="w-12 h-12 rounded-full cursor-pointer bg-black text-white flex justify-center items-center"><FcGoogle className="text-2xl" /></div>)}
+                {isLoggedIn && (<>
+                    <div onClick={() => setToggle(!toggle)} className="w-12 h-12 rounded-full cursor-pointer bg-black text-white flex justify-center items-center"><UserOutlined className="text-2xl" /></div>
 
-            <div className={`flex flex-col items-center overflow-hidden transition-all duration-300 ease-in-out ${toggle ? "max-h-60 opacity-100" : "max-h-0 opacity-0"}`}>
-                <button className="bg-black transition-all mt-4 text-white cursor-pointer hover:bg-[#F1F0E9] border-2 hover:text-black hover:border-black px-5 py-2 rounded-lg">Profile</button>
-                <button className="bg-black transition-all mt-4 text-white cursor-pointer hover:bg-[#F1F0E9] border-2 hover:text-black hover:border-black px-5 py-2 rounded-lg">Create</button>
+                    <div className={`flex flex-col items-center overflow-hidden transition-all duration-300 ease-in-out ${toggle ? "max-h-60 opacity-100" : "max-h-0 opacity-0"}`}>
+                        <button className="bg-black transition-all mt-4 text-white cursor-pointer hover:bg-[#F1F0E9] border-2 hover:text-black hover:border-black px-5 py-2 rounded-lg">Profile</button>
+                        <button className="bg-black transition-all mt-4 text-white cursor-pointer hover:bg-[#F1F0E9] border-2 hover:text-black hover:border-black px-5 py-2 rounded-lg">Create</button>
+                        <button onClick={() => logOut()} className="bg-[red] transition-all mt-4 text-white cursor-pointer hover:bg-[#F1F0E9] border-2 hover:text-black hover:border-[red] px-5 py-2 rounded-lg">Logout</button>
+                    </div>
+                </>)}
             </div>
-        </div>
+        </>
     )
 }
