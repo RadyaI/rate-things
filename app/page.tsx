ad89e802a9855f907aa6534dbf09fa0505a19b74
RadyaI <radyaiftikhar@gmail.com>
2025-03-07 22:01:17 +0700

    Up up up

--- app/page.tsx
+++ app/page.tsx
@@ -5,10 +5,11 @@ export default function Home() {
   return (
     <>
       <div className="w-9/12 p-5 mx-auto mt-8">
-        <p className="text-center text-5xl font-bold tracking-wider font-[chewy]">Rate anything you want</p>
+        <p className="hidden sm:block text-center text-5xl font-bold tracking-wider font-[chewy]">⭐ Rate anything you want ⭐</p>
+        <p className="sm:hidden text-center text-5xl font-bold tracking-wider font-[chewy]">Rate anything you want</p>
       </div>
       <Suspense fallback={<div className="text-center">Sabar yaa...</div>}>
-        <Content />
+        <Content/>
       </Suspense>
     </>
   )
--- components/root/content.tsx
+++ components/root/content.tsx
@@ -6,9 +6,9 @@ import Image from "next/image"
 export default function Content() {
     return (
         <>
-            <div className="w-[90%] mx-auto mt-4 p-10 shadow flex flex-wrap">
+            <div className="w-[90%] mx-auto mt-4 p-10 flex gap-5 flex-wrap">
 
-                <div className="w-1/3 h-[350px] p-4 cursor-pointer hover:scale-[1.1] transition-all">
+                <div className="w-full m-1 sm:w-[300px] bg-[#FBF8EF] rounded-2xl shadow-md h-[350px] p-4 cursor-pointer hover:scale-[1.1] transition-all">
                     <div className="w-full flex gap-3 font-semibold select-none">
                         <p className="bg-amber-400 shadow shadow-[grey] p-1 px-2 rounded-md">{new Date().toLocaleString("id-ID", { day: "2-digit", month: "short", year: "numeric" })}</p>
                         <p className="bg-green-400 shadow shadow-[grey] p-1 px-2 rounded-md">Activity</p>
