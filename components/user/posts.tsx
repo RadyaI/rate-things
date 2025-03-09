362b1b3239a46f66e01654d9b74c265ec8517b37
RadyaI <radyaiftikhar@gmail.com>
2025-03-09 13:04:22 +0700

    feat: delete function

--- components/user/posts.tsx
+++ components/user/posts.tsx
@@ -1,12 +1,13 @@
 "use client"
 
 import { auth, db } from "@/config/firebase"
-import { StarFilled } from "@ant-design/icons"
+import { StarFilled, DeleteFilled } from "@ant-design/icons"
 import { onAuthStateChanged } from "firebase/auth"
-import { collection, onSnapshot, orderBy, query, where } from "firebase/firestore"
+import { collection, deleteDoc, doc, onSnapshot, orderBy, query, where } from "firebase/firestore"
 import Image from "next/image"
 import { useEffect, useState } from "react"
 import { toast, ToastContainer } from "react-toastify"
+import swal from "sweetalert"
 
 type Things = {
     id: string,
@@ -45,6 +46,27 @@ export default function UserPosts() {
         }
     }
 
+    async function deletePost(postId: string) {
+        try {
+            const alert = await swal({
+                icon: 'warning',
+                title: "Delete this thing?",
+                buttons: ["No", "Yes"]
+            })
+
+            if (alert) {
+                const docRef = doc(db, "things", postId)
+                await deleteDoc(docRef)
+                toast.success("Deleted successfully")
+            }
+        } catch (error: unknown) {
+            if (error instanceof Error) {
+                console.log(error.message)
+                toast.error("Somethings went wrong!")
+            }
+        }
+    }
+
     useEffect(() => {
         const subs = onAuthStateChanged(auth, (user) => {
             if (user) {
@@ -64,10 +86,11 @@ export default function UserPosts() {
             <div className="w-full sm:w-[90%] mx-auto mt-4 p-5 sm:p-10 flex justify-center gap-5 flex-wrap">
 
                 {thingsData?.map((i, index) =>
-                    <div key={index} className="w-full m-1 sm:w-[300px] bg-[#FBF8EF] rounded-2xl shadow-md h-[350px] p-4 cursor-pointer hover:scale-[1.1] transition-all">
+                    <div key={index} className="w-full m-1 sm:w-[350px] bg-[#FBF8EF] rounded-2xl shadow-md h-[350px] p-4 cursor-pointer hover:scale-[1.1] transition-all">
                         <div className="w-full flex gap-3 font-semibold select-none">
                             <p className="bg-amber-400 shadow shadow-[grey] p-1 px-2 rounded-md">{new Date(i.createdAt).toLocaleString("id-ID", { day: "2-digit", month: "short", year: "numeric" })}</p>
                             <p className="bg-green-400 shadow shadow-[grey] p-1 px-2 rounded-md">{i.tag}</p>
+                            <p onClick={() => deletePost(i.id)} className="bg-red-400 shadow shadow-[grey] p-1 px-2 rounded-md"><DeleteFilled></DeleteFilled></p>
                         </div>
                         <div className="w-full mb-2 h-[60%] relative mt-3">
                             <Image src={i.file} sizes="100%" priority={true} fill className="select-none object-cover hover:object-contain rounded-2xl" alt="Photo"></Image>
