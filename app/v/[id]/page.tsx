3f1bbd2fb57fdd38cfb4a5be2ada766d011fea0a
RadyaI <radyaiftikhar@gmail.com>
2025-03-10 11:46:42 +0700

    feat: display comment

--- app/v/[id]/page.tsx
+++ app/v/[id]/page.tsx
@@ -1,3 +1,4 @@
+import Comment from "@/components/v/comment";
 import Rate from "@/components/v/rate";
 import { db } from "@/config/firebase";
 import { doc, getDoc } from "firebase/firestore";
@@ -18,7 +19,7 @@ type Things = {
 async function getThings(thingsId: string): Promise<Things> {
     try {
         const data = await getDoc(doc(db, "things", thingsId))
-        return {...data.data(), id: data.id} as Things
+        return { ...data.data(), id: data.id } as Things
     } catch (error: unknown) {
         if (error instanceof Error) {
             console.log(error.message)
@@ -48,7 +49,7 @@ export default async function ThingsView({ params }: { params: Promise<{ id: str
             <div className="w-6/8 h-50 sm:h-80 sm:w-1/2 mt-5 mx-auto relative">
                 <Image className="mt-3 object-contain mx-auto rounded-2xl" fill src={thingsData.file} alt="Image"></Image>
             </div>
-            <div className="w-6/8 sm:w-1/2 mb-10 mt-10 mx-auto bg-amber-50 p-5 rounded-2xl shadow shadow-black">
+            <div className="w-6/8 sm:w-1/2 mt-10 mx-auto bg-amber-50 p-5 rounded-2xl shadow shadow-black">
                 <div className="flex flex-wrap mb-3 gap-4 font-semibold">
                     <p className="bg-amber-400 p-1 px-2 rounded-md">{new Date(thingsData.createdAt).toLocaleString("id-ID", {
                         day: "2-digit",
@@ -62,6 +63,8 @@ export default async function ThingsView({ params }: { params: Promise<{ id: str
                 <p className="font-semibold text-2xl">{thingsData.title}</p>
                 <p className="mt-2 text-[grey]">{thingsData.desc}</p>
             </div>
+
+            <Comment getThingsId={thingsData}></Comment>
         </>
     )
 }
---
+++ components/v/comment.tsx
@@ -0,0 +1,75 @@
+"use client"
+
+import { db } from "@/config/firebase"
+import { collection, getDocs, orderBy, query, where } from "firebase/firestore"
+import { useEffect, useState } from "react"
+
+type Things = {
+    id: string
+    rating: number,
+    thingsId: string,
+    userRateId: string,
+    komen: string,
+    createdAt: number
+} | null
+
+export default function Comment({ getThingsId }: { getThingsId: { id: string } }) {
+
+    const [ratingData, setRatingData] = useState<Things[] | null>(null)
+
+
+    useEffect(() => {
+        async function getRating() {
+            try {
+                const get = await getDocs(query(
+                    collection(db, "rating"),
+                    where("thingsId", "==", getThingsId.id),
+                    orderBy("createdAt", "desc")
+                ))
+                const temp: Things[] = []
+                get.forEach((data) => {
+                    temp.push({ ...data.data(), id: data.id } as Things)
+                })
+                setRatingData(temp)
+    
+            } catch (error: unknown) {
+                if (error instanceof Error) {
+                    console.log(error.message)
+                }
+            }
+        }
+
+        getRating()
+    }, [getThingsId.id])
+
+    if (!ratingData) {
+        return (
+            <div className="w-6/8 sm:w-1/2 mb-10 mx-auto mt-5 p-5 bg-amber-50 rounded-2xl shadow shadow-black">
+                <small>Comment</small>
+                <p className="text-center">Sabar yaa datanya lagi diambil...</p>
+            </div>
+        )
+    }
+    
+
+    return (
+        <>
+            <div className="w-6/8 sm:w-1/2 mb-10 mx-auto mt-5 p-5 bg-amber-50 rounded-2xl shadow shadow-black">
+                <small>Comment</small>
+                {ratingData?.map((i, index) =>
+                    <div key={index} className="mt-4 shadow p-3 rounded-xl">
+                        <p className="font-semibold">{i?.komen}</p>
+                        <small className="text-[grey]">{new Date(i?.createdAt ?? 0).toLocaleString("id-ID", {
+                            day: "2-digit",
+                            month: "short",
+                            year: "numeric",
+                            minute: "2-digit",
+                            hour: "2-digit",
+                            hour12: false
+                        })}</small>
+                    </div>
+                )}
+            </div>
+        </>
+    )
+}
